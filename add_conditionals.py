86b106952ce204650f10a5025427076c3891e6b5
AnkitaDeshmukh <42597079+AnkitaDeshmukh@users.noreply.github.com>
2018-08-23 16:58:23 -0700

    Add files via upload

---
+++ add_conditionals.py
@@ -0,0 +1,10 @@
+#Initialise offset
+offset = -6
+#code the while loop
+while offset != 0:
+    print("correcting...")
+    if offset>0:
+        offset = offset - 1
+    else:
+        offset = offset + 1 
+    print(offset)
