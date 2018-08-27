b64676516b30bd9e704c7a61c6608a9c63fe8212
AnkitaDeshmukh <42597079+AnkitaDeshmukh@users.noreply.github.com>
2018-08-27 16:32:34 -0700

    Add files via upload

---
+++ determine_your_next_move.py
@@ -0,0 +1,28 @@
+
+#Determine your next move
+#In the Empire State Building bet, your next move depends on the number of eyes you throw with the dice.
+# We can perfectly code this with an if-elif-else construct!
+
+#The sample code assumes that you're currently at step 50. 
+
+# Import numpy and set seed
+import numpy as np
+np.random.seed(123)
+
+# Starting step
+step = 50
+
+# Roll the dice
+dice = np.random.randint(1,7)
+
+# Finish the control construct
+if dice <= 2 :
+    step = step - 1
+elif dice <= 5 :
+    step = step + 1
+else :
+    step = step + np.random.randint(1,7)
+
+# Print out dice and step
+print(dice)
+print(step)
